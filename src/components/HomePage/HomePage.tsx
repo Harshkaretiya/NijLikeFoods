0700b3b42bc6e4028a87bcf58a509b89bbff36bd
Harshkaretiya <110233351+Harshkaretiya@users.noreply.github.com>
2025-03-09 19:15:45 +0530

    updated

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <section
       id="Home"
-      className="w-full h-screen md:bg-[url('/background3.png')] bg-[url('/background4.png')] bg-cover bg-center relative "
+      className="w-full h-screen md:bg-[url('/background4.png')] bg-[url('/background4.png')] bg-cover bg-center relative "
     >
       <div className="absolute hidden lg:flex top-[35%] left-[12%] flex-col gap-10 justify-center items-center">
         <img src="./full-logo.png" className="w-100" alt="" />
